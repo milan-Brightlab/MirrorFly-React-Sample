6e521dd7c9514e292db258846686283510dde9f8
Ubuntu <ubuntu@ip-172-31-46-91.eu-west-1.compute.internal>
2024-11-26 09:05:51 +0000

    Update environment configs, fix assets, and cleanup

--- src/Components/Layouts/OtpLogin/firebaseConfig.js
+++ src/Components/Layouts/OtpLogin/firebaseConfig.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    "apiKey": "AIzaSyBsDgqVCPQkReMH5BlmXGxRirPYzKgSdE0",
-    "authDomain": "mirrorfly-uikit.firebaseapp.com",
-    "databaseURL": "",
-    "projectId": "mirrorfly-uikit",
-    "storageBucket": "",
-    "messagingSenderId": ""
-};
+    apiKey: "AIzaSyDrRkpCoYgYHrBhOo5RrlmR-Qs0-0vYrVA",
+    authDomain: "mirrorfly-demo-app-ad399.firebaseapp.com",
+    projectId: "mirrorfly-demo-app-ad399",
+    storageBucket: "mirrorfly-demo-app-ad399.appspot.com",
+    messagingSenderId: "1004690942780",
+    appId: "1:1004690942780:web:af1fa40a5c5d3a26110c39",
+    measurementId: "G-TVZJC7NNYD"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
